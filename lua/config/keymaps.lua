d67fc93c55a621cd9c8301aa171e0ceb32233a6a
Felyp-Henrique <felyphenrique2015@gmail.com>
2023-12-30 18:41:15 -0300

    conf: added others shortcuts to debug

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,5 +3,7 @@
 -- Add any additional keymaps here
 vim.api.nvim_set_keymap('n', '<C-d>', '<Cmd>lua require("dapui").toggle()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<F5>', '<Cmd>lua require("dap").continue()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<F6>', '<Cmd>lua require("dap").step_into()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<F7>', '<Cmd>lua require("dap").step_out()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<F9>', '<Cmd>lua require("dap").toggle_breakpoint()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<F10>', '<Cmd>lua require("dap").step_over()<CR>', { noremap = true, silent = true })
